2f02ea7a700b710571076c162952dc897f89a65c
Parker <parker.soravee@gmail.com>
2021-11-21 15:31:06 +0700

    build web

--- web-build/flutter_service_worker.js
+++ web-build/flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d596ac2d3348988966a74b160cfe5f88",
-"/": "d596ac2d3348988966a74b160cfe5f88",
+"index.html": "4d2cebe74a4e3c8be431ead8710bd714",
+"/": "4d2cebe74a4e3c8be431ead8710bd714",
 "main.dart.js": "86942b950002e24066b9ec38fdaa217b",
 "manifest.json": "2488f5493e8cef4fed551294c577312a",
 "version.json": "e20e92e4f66be5e58df0e1bd9ff65310"
